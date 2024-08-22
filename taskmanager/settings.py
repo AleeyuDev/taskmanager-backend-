d45b2bd46b0ebd8d7b3b864cbf3942eb8d8dc4e3
Aliyu idris. <aleeyuidi@gmail.com>
2024-08-23 00:50:20 +0100

    Update settings.py

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -80,14 +80,14 @@ WSGI_APPLICATION = 'taskmanager.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+ https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+ DATABASES = {
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+ }
 
 
 # Password validation
