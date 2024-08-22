f93c6cf4a440177598445cabd657283e4913a7f3
Aliyu idris. <aleeyuidi@gmail.com>
2024-08-23 00:54:55 +0100

    Update settings.py

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
